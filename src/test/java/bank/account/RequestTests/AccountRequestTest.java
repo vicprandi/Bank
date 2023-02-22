ea2355a23677ae3104425d9fb9775f1576486cf6
vicprandi <vicprandi@gmail.com>
2023-02-22 14:18:33 -0300

    AccountRequest Test

---
+++ src/test/java/bank/account/RequestTests/AccountRequestTest.java
@@ -0,0 +1,20 @@
+package bank.account.RequestTests;
+
+import bank.account.request.AccountRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+public class AccountRequestTest {
+
+    @Test
+    public void AccountRequestTest() {
+        AccountRequest accountRequest = new AccountRequest();
+        accountRequest.setBalanceMoney(BigDecimal.valueOf(100.00));
+
+        Assertions.assertEquals(BigDecimal.valueOf(100.00), accountRequest.getBalanceMoney());
+
+    }
+
+}
