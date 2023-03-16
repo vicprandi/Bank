a2a49429455dfbc3b5b15b49e61b0695484572c9
vicprandi <vicprandi@gmail.com>
2023-03-16 15:10:04 -0300

    Puxando o localhost através do @Value ao invés de colocá-lo diretamente na configuração.

--- src/main/resources/static/kafka/KafkaConfiguration.java
+++ src/main/resources/static/kafka/KafkaConfiguration.java
@@ -3,6 +3,7 @@ package bank.kafka;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Collections;
@@ -11,18 +12,18 @@ import java.util.Properties;
 @Configuration
 public class KafkaConfiguration {
 
-    public KafkaConfiguration() {
-        createTopic();
+    public KafkaConfiguration(@Value("${bootstrap-servers}") String bootstrapServers) {
+        createTopic(bootstrapServers);
     }
 
     /***
     Essa classe será executada ao iniciar o aplicativo Spring e criará o tópico chamado "transactions" com 1 partição e um fator de replicação de 1.
     Essa operação deve ser realizada apenas uma vez antes de começar a produzir ou consumir mensagens no Kafka.
-    ***/
+     ***/
 
-    private void createTopic() {
+    private void createTopic(String bootstrapServers) {
         Properties props = new Properties();
-        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 
         AdminClient adminClient = AdminClient.create(props);
 
