2e3062f68cc650a5caa264abc704ac77c1a06e16
vicprandi <vicprandi@gmail.com>
2023-03-20 22:03:24 -0300

    Está dando erro para subir a aplicação.

--- src/main/java/bank/kafka/KafkaConfiguration.java
+++ src/main/java/bank/kafka/KafkaConfiguration.java
@@ -3,20 +3,27 @@ package bank.kafka;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
-/***
- Essa classe será executada ao iniciar o aplicativo Spring e criará o tópico chamado "transactions" com 1 partição e um fator de replicação de 1.
- Essa operação deve ser realizada apenas uma vez antes de começar a produzir ou consumir mensagens no Kafka.
- Eu tinha colocado o Configuration no lugar errado!
- ***/
-
 @Configuration
+@EnableKafka
 @PropertySource("classpath:application.properties")
 public class KafkaConfiguration {
 
@@ -26,9 +33,22 @@ public class KafkaConfiguration {
      * docker exec bank kafka-topics --describe --topic transactions --bootstrap-server localhost:9092
      ***/
 
+    @Value("${bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${topic.name}")
+    private String topicName;
+
     public KafkaConfiguration(@Value("${bootstrap-servers}") String bootstrapServers,
                               @Value("${topic.name}") String topicName) {
         createTopic(bootstrapServers, topicName);
+    }
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return new KafkaAdmin(configs);
     }
 
     private void createTopic(String bootstrapServers, String topicName) {
